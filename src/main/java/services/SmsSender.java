caac37b40838f8fa876ab007b4b73f832c4952c6
Mohamed amine abdellah <aminebenmohamedabdellah@gmail.com>
2025-05-01 23:32:31 +0100

    api+metier

--- src/main/java/services/SmsSender.java
+++ src/main/java/services/SmsSender.java
@@ -4,5 +4,18 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class SmsSender {
+    // Replace these with your actual Twilio credentials
+    public static final String ACCOUNT_SID = "ACd914e6e7d81fd9cfbd3a72f9528a6816";
+    public static final String AUTH_TOKEN = "07ea8c71f626e2bacb3131cf7608710a";
+    public static final String TWILIO_NUMBER = "+17756407607"; // Your Twilio phone number
 
+    public static void sendSms(String toNumber, String message) {
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+        Message.creator(
+                new PhoneNumber(toNumber),  // To number
+                new PhoneNumber(TWILIO_NUMBER),  // From Twilio number
+                message
+        ).create();
+    }
 }
